2654479a783a4c0bf522c791349f0a58fa215cb5
fazalshahzad <shancompany322@gmail.com>
2023-04-28 21:51:34 +0500

    c

--- Configration/DatabaseConfigration.js
+++ Configration/DatabaseConfigration.js
@@ -1,18 +1,14 @@
-const Package = require("../package.json");
-const mongoose= require("mongoose");
-mongoose.connect(`${process.env.MONGO_URI}`,{useNewUrlParser:true,useUnifiedTopology:true},(error,connection)=>{  
-    if(!error){
-console.log(`\nMongoDb connected SucessFully at MongoAtlas WithDatabBase\n`);
-console.log(`\nYour ap has following Dependencies\n`);
-for(let dependencies in Package.dependencies){      //loop in object keys so use for in loop
-    console.log(dependencies);
-}
+const mongoose = require('mongoose');
 
-    }
-    else {console.log(`Error:Not Connected to the MongoDb` + error)}
+const MONGO_URI = 'mongodb+srv://fazal:Iphone1122@cluster0.uhgzgkg.mongodb.net/mytestingwebsite?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-
-// useNewUrlParser:true (protocols or database rules ) allow to convert BSON (Binary-javascript-object-notation) (0-1 form) Data to Json
-
-//mongoose.connect('mongodb+srv://ecom-123:ecom-123@e-commerce.pgkqfrl.mongodb.net/E-Commerce?retryWrites=true&w=majority',
-//{useNewUrlParser:true,useUnifiedTopology:true},(error,connection)=>{ 1st step without dev.env 
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB', error);
+});
